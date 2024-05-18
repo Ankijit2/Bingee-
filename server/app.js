7cd062c9e99ef46d3c9a1009350f39bb2a4e17cd
Ankijit2 <ankijit714@gmail.com>
2024-05-18 08:13:46 +0530

    Done changes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { connectDB } from "./utils/features.js";
-import dotenv from "dotenv";
+import "dotenv/config";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import { Server } from "socket.io";
@@ -26,9 +26,7 @@ import userRoute from "./routes/user.js";
 import chatRoute from "./routes/chat.js";
 import adminRoute from "./routes/admin.js";
 
-dotenv.config({
-  path: "./.env",
-});
+
 
 const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
