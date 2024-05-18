f3c5cb9da6c7c0e89037857bdb4e6c42f09e9b9c
Ankijit2 <ankijit714@gmail.com>
2024-05-18 08:22:15 +0530

    changes done

--- server/utils/features.js
+++ server/utils/features.js
@@ -13,8 +13,10 @@ const cookieOptions = {
 
 const connectDB = async() => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_CONNECT_URI);
-    console.log(`MongoDB connected: ${connection.connection.host}`);
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+   
+    console.log("MongoDB connected");
+   
 } catch (error) {
     console.log(error);
     process.exit(1)
